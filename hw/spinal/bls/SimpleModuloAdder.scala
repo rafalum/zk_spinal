7bc6af45009212adb835fdf8916c7a277dfc0d60
rafalum <de.rafael@pm.me>
2024-02-19 14:59:55 +0100

    simple modulo adder/subtractor

---
+++ hw/spinal/bls/SimpleModuloAdder.scala
@@ -0,0 +1,20 @@
+package bls
+
+import spinal.core._
+
+class SimpleModuloAdder(N: Int, MOD_PRIME: Int) extends Component {
+
+  val io = new Bundle {
+    val a = in UInt(N bits)
+    val b = in UInt(N bits)
+    val o = out UInt(N bits)
+  }
+
+
+  when(io.b > io.a) {
+    io.o := IntToUInt(MOD_PRIME).resize(N bits) - (io.b - io.a)
+  }.otherwise {
+    io.o := (io.a - io.b) % IntToUInt(MOD_PRIME).resize(N bits)
+  }
+
+}
